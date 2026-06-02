ecf285a9846f030687f16bbd55fbc96bdf0175eb
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-06-02 16:39:59 -0300

    restart project

--- backend/migrations/02_add_performance_indexes.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX idx_notes_status ON notes(status);
-CREATE INDEX idx_notes_attributes_gin ON notes USING gin (attributes);
-CREATE INDEX idx_notes_flow_created ON notes(flow_id, created_at DESC);
